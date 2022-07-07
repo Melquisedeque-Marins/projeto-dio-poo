a4ac8357c1a4d4b74b12aef07fe7f17d62c15674
melck <melck_junior@hotmail.com>
2022-07-07 16:49:17 +0100

    criação classes Curso e Mentoria

--- src/App.java
+++ src/App.java
@@ -1,5 +1,29 @@
+import java.time.LocalDate;
+
+import domain.Curso;
+import domain.Mentoria;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        
+        Curso curso1 = new Curso();
+
+        curso1.setTitulo("java");
+        curso1.setDescricao("descricao curso java");
+        curso1.setCargaHoraria(8);
+
+        Curso curso2 = new Curso();
+
+        curso2.setTitulo("java script");
+        curso2.setDescricao("descricao curso java script");
+        curso2.setCargaHoraria(16);
+
+        Mentoria mentoria = new Mentoria();
+        mentoria.setTitulo("mentoria java");
+        mentoria.setDescricao("descrição mentoria java");
+        mentoria.setData(LocalDate.now());
+
+        System.out.println(curso1);
+
     }
 }
